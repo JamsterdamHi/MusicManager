c04c705573c2c43ef2f38fa91a0c2f03388d3426
JamsterdamHi <112298582+JamsterdamHi@users.noreply.github.com>
2022-12-24 13:18:06 +0900

    マイグレーション作成

---
+++ database/migrations/2022_12_24_132801_create_songs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 140)->index();
+            $table->string('youtube_url')->nullable();
+            $table->foreignId('artist_id')->constrained('artists');
+            $table->foreignId('genre_id')->constrained('genres');
+            $table->foreignId('mood_id')->constrained('moods');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
---
+++ database/migrations/2022_12_24_134934_create_playlist_song_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaylistSongTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlist_song', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->foreignId('song_id')->constrained('songs');
+            $table->foreignId('playlist_id')->constrained('playlists');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlist_song');
+    }
+}
