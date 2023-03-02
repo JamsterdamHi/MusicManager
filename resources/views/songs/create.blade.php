b93fd985c8a74338263fbd8f7bbca0b4238372c4
JamsterdamHi <112298582+JamsterdamHi@users.noreply.github.com>
2023-03-02 22:55:33 +0900

    migrate:fresh, genre,mood名修正

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -38,16 +38,24 @@
                         <div class="col">
                             <label for="custom-select-1">ムード</label>
                             <select id="mood" name="mood_id" class="custom-select">
-                                <option value="1" {{ old('mood_id')==1 ? 'selected' : '' }}>アップテンポ</option>
-                                <option value="2" {{ old('mood_id')==2 ? 'selected' : '' }}>ミディアムテンポ</option>
-                                <option value="3" {{ old('mood_id')==3 ? 'selected' : '' }}>バラード</option>
-                                <option value="4" {{ old('mood_id')==4 ? 'selected' : '' }}>和風</option>
-                                <option value="5" {{ old('mood_id')==5 ? 'selected' : '' }}>神聖な</option>
-                                <option value="6" {{ old('mood_id')==6 ? 'selected' : '' }}>悲しい</option>
-                                <option value="7" {{ old('mood_id')==7 ? 'selected' : '' }}>激しい</option>
-                                <option value="8" {{ old('mood_id')==8 ? 'selected' : '' }}>ドラマティック</option>
-                                <option value="9" {{ old('mood_id')==9 ? 'selected' : '' }}>ゲーム余興</option>
-                                <option value="10" {{ old('mood_id')==10 ? 'selected' : '' }}>効果音</option>
+                                <option value="1" {{ old('mood_id')==1 ? 'selected' : '' }}>明るい</option>
+                                <option value="2" {{ old('mood_id')==2 ? 'selected' : '' }}>幸せ</option>
+                                <option value="3" {{ old('mood_id')==3 ? 'selected' : '' }}>ドラマティック</option>
+                                <option value="4" {{ old('mood_id')==4 ? 'selected' : '' }}>神聖</option>
+                                <option value="5" {{ old('mood_id')==5 ? 'selected' : '' }}>パワフル</option>
+                                <option value="6" {{ old('mood_id')==6 ? 'selected' : '' }}>面白い</option>
+                                <option value="7" {{ old('mood_id')==7 ? 'selected' : '' }}>愛</option>
+                                <option value="8" {{ old('mood_id')==8 ? 'selected' : '' }}>友情</option>
+                                <option value="9" {{ old('mood_id')==9 ? 'selected' : '' }}>家族</option>
+                                <option value="10" {{ old('mood_id')==10 ? 'selected' : '' }}>静かな</option>
+                                <option value="11" {{ old('mood_id')==11 ? 'selected' : '' }}>都会的</option>
+                                <option value="12" {{ old('mood_id')==12 ? 'selected' : '' }}>セクシー</option>
+                                <option value="13" {{ old('mood_id')==13 ? 'selected' : '' }}>和風</option>
+                                <option value="14" {{ old('mood_id')==14 ? 'selected' : '' }}>切ない</option>
+                                <option value="15" {{ old('mood_id')==15 ? 'selected' : '' }}>ミステリアス</option>
+                                <option value="16" {{ old('mood_id')==16 ? 'selected' : '' }}>暗い</option>
+                                <option value="17" {{ old('mood_id')==17 ? 'selected' : '' }}>怒り</option>
+                                <option value="18" {{ old('mood_id')==18 ? 'selected' : '' }}>ゲーム</option>
                             </select>
                         </div>
 
@@ -56,14 +64,26 @@
                             <select id="genre" name="genre_id" class="custom-select">
                                 <option value="1" {{ old('genre_id')==1 ? 'selected' : '' }}>ポップス</option>
                                 <option value="2" {{ old('genre_id')==2 ? 'selected' : '' }}>ロック</option>
-                                <option value="3" {{ old('genre_id')==3 ? 'selected' : '' }}>ダンス</option>
-                                <option value="4" {{ old('genre_id')==4 ? 'selected' : '' }}>ヒップホップ</option>
-                                <option value="5" {{ old('genre_id')==5 ? 'selected' : '' }}>R＆B</option>
-                                <option value="6" {{ old('genre_id')==6 ? 'selected' : '' }}>レゲエ</option>
-                                <option value="7" {{ old('genre_id')==7 ? 'selected' : '' }}>ジャズ</option>
-                                <option value="8" {{ old('genre_id')==8 ? 'selected' : '' }}>クラシック</option>
-                                <option value="9" {{ old('genre_id')==9 ? 'selected' : '' }}>インスト</option>
-                                <option value="10" {{ old('genre_id')==10 ? 'selected' : '' }}>オルゴール</option>
+                                <option value="3" {{ old('genre_id')==3 ? 'selected' : '' }}>ヒップホップ</option>
+                                <option value="4" {{ old('genre_id')==4 ? 'selected' : '' }}>R＆B/ソウル</option>
+                                <option value="5" {{ old('genre_id')==5 ? 'selected' : '' }}>レゲエ</option>
+                                <option value="6" {{ old('genre_id')==6 ? 'selected' : '' }}>ダンス</option>
+                                <option value="7" {{ old('genre_id')==7 ? 'selected' : '' }}>テクノ</option>
+                                <option value="8" {{ old('genre_id')==8 ? 'selected' : '' }}>アンビエント</option>
+                                <option value="9" {{ old('genre_id')==9 ? 'selected' : '' }}>フォーク</option>
+                                <option value="10" {{ old('genre_id')==10 ? 'selected' : '' }}>ブルース</option>
+                                <option value="11" {{ old('genre_id')==11 ? 'selected' : '' }}>ジャズ</option>
+                                <option value="12" {{ old('genre_id')==12 ? 'selected' : '' }}>カントリー</option>
+                                <option value="13" {{ old('genre_id')==13 ? 'selected' : '' }}>ラテン</option>
+                                <option value="14" {{ old('genre_id')==14 ? 'selected' : '' }}>ワールド</option>
+                                <option value="15" {{ old('genre_id')==15 ? 'selected' : '' }}>ボカロ</option>
+                                <option value="16" {{ old('genre_id')==16 ? 'selected' : '' }}>クラシック</option>
+                                <option value="17" {{ old('genre_id')==17 ? 'selected' : '' }}>子供</option>
+                                <option value="18" {{ old('genre_id')==18 ? 'selected' : '' }}>演歌</option>
+                                <option value="19" {{ old('genre_id')==19 ? 'selected' : '' }}>合唱</option>
+                                <option value="20" {{ old('genre_id')==20 ? 'selected' : '' }}>インスト</option>
+                                <option value="21" {{ old('genre_id')==21 ? 'selected' : '' }}>オルゴール</option>
+                                <option value="22" {{ old('genre_id')==22 ? 'selected' : '' }}>効果音</option>
                             </select>
                         </div>
                     </div>
